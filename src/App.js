512db24926c4501a78366b95ba63d146dc312ed6
flukekazo55 <44312069+flukekazo55@users.noreply.github.com>
2019-10-15 14:26:39 +0700

    real case

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo_partner.jpeg';
+import photo_teacher from './photo_teacher.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+		<h1>
+          Subject 88832359-OS
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload Part 2.
+		<p>
+          Hello Peerasak Pianprasit
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/peerasakp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link Teacher's Facebook 
         </a>
       </header>
     </div>
